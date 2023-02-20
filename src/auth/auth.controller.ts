d4b160aa6544eaef899d8a6ba87c8da18547a148
apple <apple@apples-MacBook-Pro.local>
2023-02-20 17:26:04 +0500

    working on passport and jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,35 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from 'src/shared/user.service';
+import { Payload } from 'src/types/payload';
+import { LoginDto, RegisterDto } from './auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+  ) {}
 
   @Post('login')
-  async login(@Body() userDto: any) {
-    return await this.userService.findByLogin(userDto);
+  async login(@Body() userDTO: LoginDto) {
+    const user = await this.userService.findByLogin(userDTO);
+    const payload: Payload = {
+      username: user.username,
+      seller: user.seller,
+    };
+    const token = await this.authService.signPayload(payload);
+    return { user, token };
   }
 
   @Post('register')
-  async register(@Body() userDto: any) {
-    return await this.userService.create(userDto);
+  async register(@Body() userDTO: RegisterDto) {
+    const user = await this.userService.create(userDTO);
+    const payload: Payload = {
+      username: user.username,
+      seller: user.seller,
+    };
+    const token = await this.authService.signPayload(payload);
+    return { user, token };
   }
 }
